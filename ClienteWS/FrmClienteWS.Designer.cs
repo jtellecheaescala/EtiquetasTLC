993b33fa6b70a1f919b625c3c1075004e6efa8d8
jtellecheaescala <71897263+jtellecheaescala@users.noreply.github.com>
2023-07-05 18:56:04 -0300

    Agregado de configuracion cliente WS

--- ClienteWS/FrmClienteWS.Designer.cs
+++ ClienteWS/FrmClienteWS.Designer.cs
@@ -40,7 +40,6 @@
             this.lblUser = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.txtIdRemito = new System.Windows.Forms.TextBox();
-            this.txtSepararPorDoc = new System.Windows.Forms.TextBox();
             this.txtSize = new System.Windows.Forms.TextBox();
             this.txtFormat = new System.Windows.Forms.TextBox();
             this.txtCliente = new System.Windows.Forms.TextBox();
@@ -48,8 +47,11 @@
             this.txtTemplate = new System.Windows.Forms.TextBox();
             this.txtRutaExportacion = new System.Windows.Forms.TextBox();
             this.btnInvocar = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
             this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtSepararPorDocumento = new System.Windows.Forms.MaskedTextBox();
+            this.txtURLWebService = new System.Windows.Forms.TextBox();
+            this.lblUrlServicio = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -64,7 +66,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(58, 97);
+            this.lblPassword.Location = new System.Drawing.Point(55, 139);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 1;
@@ -73,7 +75,7 @@
             // lblIdRemito
             // 
             this.lblIdRemito.AutoSize = true;
-            this.lblIdRemito.Location = new System.Drawing.Point(58, 123);
+            this.lblIdRemito.Location = new System.Drawing.Point(55, 165);
             this.lblIdRemito.Name = "lblIdRemito";
             this.lblIdRemito.Size = new System.Drawing.Size(49, 13);
             this.lblIdRemito.TabIndex = 2;
@@ -82,7 +84,7 @@
             // lblRutaExportacion
             // 
             this.lblRutaExportacion.AutoSize = true;
-            this.lblRutaExportacion.Location = new System.Drawing.Point(61, 279);
+            this.lblRutaExportacion.Location = new System.Drawing.Point(58, 321);
             this.lblRutaExportacion.Name = "lblRutaExportacion";
             this.lblRutaExportacion.Size = new System.Drawing.Size(89, 13);
             this.lblRutaExportacion.TabIndex = 3;
@@ -91,7 +93,7 @@
             // lblTemplate
             // 
             this.lblTemplate.AutoSize = true;
-            this.lblTemplate.Location = new System.Drawing.Point(61, 253);
+            this.lblTemplate.Location = new System.Drawing.Point(58, 295);
             this.lblTemplate.Name = "lblTemplate";
             this.lblTemplate.Size = new System.Drawing.Size(51, 13);
             this.lblTemplate.TabIndex = 4;
@@ -100,7 +102,7 @@
             // LblSize
             // 
             this.LblSize.AutoSize = true;
-            this.LblSize.Location = new System.Drawing.Point(61, 201);
+            this.LblSize.Location = new System.Drawing.Point(58, 243);
             this.LblSize.Name = "LblSize";
             this.LblSize.Size = new System.Drawing.Size(27, 13);
             this.LblSize.TabIndex = 5;
@@ -109,7 +111,7 @@
             // lblCliente
             // 
             this.lblCliente.AutoSize = true;
-            this.lblCliente.Location = new System.Drawing.Point(61, 149);
+            this.lblCliente.Location = new System.Drawing.Point(58, 191);
             this.lblCliente.Name = "lblCliente";
             this.lblCliente.Size = new System.Drawing.Size(39, 13);
             this.lblCliente.TabIndex = 6;
@@ -118,7 +120,7 @@
             // lblSepararDoc
             // 
             this.lblSepararDoc.AutoSize = true;
-            this.lblSepararDoc.Location = new System.Drawing.Point(61, 227);
+            this.lblSepararDoc.Location = new System.Drawing.Point(58, 269);
             this.lblSepararDoc.Name = "lblSepararDoc";
             this.lblSepararDoc.Size = new System.Drawing.Size(83, 13);
             this.lblSepararDoc.TabIndex = 7;
@@ -127,7 +129,7 @@
             // lblFormat
             // 
             this.lblFormat.AutoSize = true;
-            this.lblFormat.Location = new System.Drawing.Point(61, 175);
+            this.lblFormat.Location = new System.Drawing.Point(58, 217);
             this.lblFormat.Name = "lblFormat";
             this.lblFormat.Size = new System.Drawing.Size(39, 13);
             this.lblFormat.TabIndex = 8;
@@ -136,7 +138,7 @@
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(58, 70);
+            this.lblUser.Location = new System.Drawing.Point(55, 112);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(29, 13);
             this.lblUser.TabIndex = 9;
@@ -145,7 +147,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(454, 41);
+            this.label11.Location = new System.Drawing.Point(536, 41);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(76, 13);
             this.label11.TabIndex = 10;
@@ -153,98 +155,127 @@
             // 
             // txtIdRemito
             // 
-            this.txtIdRemito.Location = new System.Drawing.Point(156, 120);
+            this.txtIdRemito.Location = new System.Drawing.Point(153, 162);
             this.txtIdRemito.Name = "txtIdRemito";
-            this.txtIdRemito.Size = new System.Drawing.Size(189, 20);
+            this.txtIdRemito.Size = new System.Drawing.Size(353, 20);
             this.txtIdRemito.TabIndex = 11;
-            // 
-            // txtSepararPorDoc
-            // 
-            this.txtSepararPorDoc.Location = new System.Drawing.Point(156, 224);
-            this.txtSepararPorDoc.Name = "txtSepararPorDoc";
-            this.txtSepararPorDoc.Size = new System.Drawing.Size(189, 20);
-            this.txtSepararPorDoc.TabIndex = 13;
+            this.txtIdRemito.Text = "6048302";
             // 
             // txtSize
             // 
-            this.txtSize.Location = new System.Drawing.Point(156, 198);
+            this.txtSize.Location = new System.Drawing.Point(153, 240);
             this.txtSize.Name = "txtSize";
-            this.txtSize.Size = new System.Drawing.Size(189, 20);
+            this.txtSize.Size = new System.Drawing.Size(353, 20);
             this.txtSize.TabIndex = 14;
+            this.txtSize.Text = "zebra";
             // 
             // txtFormat
             // 
-            this.txtFormat.Location = new System.Drawing.Point(156, 172);
+            this.txtFormat.Location = new System.Drawing.Point(153, 214);
             this.txtFormat.Name = "txtFormat";
-            this.txtFormat.Size = new System.Drawing.Size(189, 20);
+            this.txtFormat.Size = new System.Drawing.Size(353, 20);
             this.txtFormat.TabIndex = 15;
+            this.txtFormat.Text = "pdf";
             // 
             // txtCliente
             // 
-            this.txtCliente.Location = new System.Drawing.Point(156, 146);
+            this.txtCliente.Location = new System.Drawing.Point(153, 188);
             this.txtCliente.Name = "txtCliente";
-            this.txtCliente.Size = new System.Drawing.Size(189, 20);
+            this.txtCliente.Size = new System.Drawing.Size(353, 20);
             this.txtCliente.TabIndex = 16;
+            this.txtCliente.Text = "22";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(156, 94);
+            this.txtPassword.Location = new System.Drawing.Point(153, 136);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(189, 20);
+            this.txtPassword.Size = new System.Drawing.Size(353, 20);
             this.txtPassword.TabIndex = 17;
+            this.txtPassword.Text = "sgl";
             // 
             // txtTemplate
             // 
-            this.txtTemplate.Location = new System.Drawing.Point(156, 250);
+            this.txtTemplate.Location = new System.Drawing.Point(153, 292);
             this.txtTemplate.Name = "txtTemplate";
-            this.txtTemplate.Size = new System.Drawing.Size(189, 20);
+            this.txtTemplate.Size = new System.Drawing.Size(353, 20);
             this.txtTemplate.TabIndex = 12;
+            this.txtTemplate.Text = "Bultos";
             // 
             // txtRutaExportacion
             // 
-            this.txtRutaExportacion.Location = new System.Drawing.Point(156, 276);
+            this.txtRutaExportacion.Location = new System.Drawing.Point(153, 318);
             this.txtRutaExportacion.Name = "txtRutaExportacion";
-            this.txtRutaExportacion.Size = new System.Drawing.Size(189, 20);
+            this.txtRutaExportacion.Size = new System.Drawing.Size(353, 20);
             this.txtRutaExportacion.TabIndex = 18;
+            this.txtRutaExportacion.Text = "C:\\Prueba WSEtiqueta";
             // 
             // btnInvocar
             // 
-            this.btnInvocar.Location = new System.Drawing.Point(156, 324);
+            this.btnInvocar.Location = new System.Drawing.Point(153, 366);
             this.btnInvocar.Name = "btnInvocar";
             this.btnInvocar.Size = new System.Drawing.Size(146, 43);
             this.btnInvocar.TabIndex = 19;
             this.btnInvocar.Text = "Invocar WS";
             this.btnInvocar.UseVisualStyleBackColor = true;
+            this.btnInvocar.Click += new System.EventHandler(this.btnInvocar_Click);
             // 
-            // textBox2
+            // txtResultado
             // 
-            this.textBox2.Location = new System.Drawing.Point(457, 57);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(577, 366);
-            this.textBox2.TabIndex = 20;
+            this.txtResultado.Location = new System.Drawing.Point(539, 57);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(577, 366);
+            this.txtResultado.TabIndex = 20;
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(156, 67);
+            this.txtUser.Location = new System.Drawing.Point(153, 109);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(189, 20);
+            this.txtUser.Size = new System.Drawing.Size(353, 20);
             this.txtUser.TabIndex = 21;
+            this.txtUser.Text = "sgl";
+            // 
+            // txtSepararPorDocumento
+            // 
+            this.txtSepararPorDocumento.Location = new System.Drawing.Point(153, 266);
+            this.txtSepararPorDocumento.Mask = "00";
+            this.txtSepararPorDocumento.Name = "txtSepararPorDocumento";
+            this.txtSepararPorDocumento.Size = new System.Drawing.Size(353, 20);
+            this.txtSepararPorDocumento.TabIndex = 22;
+            // 
+            // txtURLWebService
+            // 
+            this.txtURLWebService.Location = new System.Drawing.Point(153, 83);
+            this.txtURLWebService.Name = "txtURLWebService";
+            this.txtURLWebService.Size = new System.Drawing.Size(353, 20);
+            this.txtURLWebService.TabIndex = 24;
+            this.txtURLWebService.Text = "http://localhost:51657/WSEtiquetas.asmx";
+            // 
+            // lblUrlServicio
+            // 
+            this.lblUrlServicio.AutoSize = true;
+            this.lblUrlServicio.Location = new System.Drawing.Point(55, 86);
+            this.lblUrlServicio.Name = "lblUrlServicio";
+            this.lblUrlServicio.Size = new System.Drawing.Size(82, 13);
+            this.lblUrlServicio.TabIndex = 23;
+            this.lblUrlServicio.Text = "Url WebService";
             // 
             // FrmClienteWS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1074, 507);
+            this.ClientSize = new System.Drawing.Size(1216, 507);
+            this.Controls.Add(this.txtURLWebService);
+            this.Controls.Add(this.lblUrlServicio);
+            this.Controls.Add(this.txtSepararPorDocumento);
             this.Controls.Add(this.txtUser);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.btnInvocar);
             this.Controls.Add(this.txtRutaExportacion);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtCliente);
             this.Controls.Add(this.txtFormat);
             this.Controls.Add(this.txtSize);
-            this.Controls.Add(this.txtSepararPorDoc);
             this.Controls.Add(this.txtTemplate);
             this.Controls.Add(this.txtIdRemito);
             this.Controls.Add(this.label11);
@@ -279,7 +310,6 @@
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox txtIdRemito;
-        private System.Windows.Forms.TextBox txtSepararPorDoc;
         private System.Windows.Forms.TextBox txtSize;
         private System.Windows.Forms.TextBox txtFormat;
         private System.Windows.Forms.TextBox txtCliente;
@@ -287,7 +317,10 @@
         private System.Windows.Forms.TextBox txtTemplate;
         private System.Windows.Forms.TextBox txtRutaExportacion;
         private System.Windows.Forms.Button btnInvocar;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtResultado;
         private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.MaskedTextBox txtSepararPorDocumento;
+        private System.Windows.Forms.TextBox txtURLWebService;
+        private System.Windows.Forms.Label lblUrlServicio;
     }
 }
--- ClienteWS/FrmClienteWS.cs
+++ ClienteWS/FrmClienteWS.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ClienteWS
 {
@@ -15,6 +17,30 @@ namespace ClienteWS
         public FrmClienteWS()
         {
             InitializeComponent();
+        }
+
+        private void btnInvocar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceReferenceWSEtiquetas.WSEtiquetasSoapClient ws = new ServiceReferenceWSEtiquetas.WSEtiquetasSoapClient();
+
+                ws.Endpoint.Address = new System.ServiceModel.EndpointAddress(txtURLWebService.Text);
+
+                int separarPorDoc = String.IsNullOrEmpty(txtSepararPorDocumento.Text) ? 0 : Convert.ToInt32(txtSepararPorDocumento.Text);
+               
+                // Eliminar los siguientes comentarios si el servicio es https.
+                //ServicePointManager.Expect100Continue = true;
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                var response = ws.EtiquetasWS(txtUser.Text, txtPassword.Text, txtIdRemito.Text, txtCliente.Text, txtFormat.Text, txtSize.Text, separarPorDoc, txtTemplate.Text);
+
+                txtResultado.Text = response.message;
+            }
+            catch (Exception ex)
+            {
+                txtResultado.Text = $"Se ha producido un error:  {ex.Message}. {Environment.NewLine} {ex.StackTrace} ";
+            }
+
         }
     }
 }
