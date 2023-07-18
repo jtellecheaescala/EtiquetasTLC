129319b444a39d8226dab3753e36782563c8664a
jtellecheaescala <71897263+jtellecheaescala@users.noreply.github.com>
2023-07-18 15:54:15 -0300

    Borrado de comentarios

--- web/App_Code/Handlers/HandlersEtiquetasApple.cs
+++ web/App_Code/Handlers/HandlersEtiquetasApple.cs
@@ -58,7 +58,6 @@ public class HandlersEtiquetasApple
         try
         {
 
-
             ruta = String.Format(@"{0}\{1}_{2}.pdf", gvalues.PathOut, etiqueta, DateTime.Now.ToString("yyyyMMdd-HHmmssffff"));
             log.GrabarLogs(connLog, severidades.MsgSoporte1, "INFO", String.Format("Inicializando handler etiquetas BULTOS_DHL_APPLE. Generando ruta: {0}", ruta));
 
@@ -70,8 +69,8 @@ public class HandlersEtiquetasApple
                 pageSize = PageSize.A4;
             }
             else
-            { // new PageSize(285, 220): representa: 100,5 x 77,6
-              // new PageSize(170, 85):  representa: a 60 x 30 
+            { 
+              // new PageSize(170, 85):  representa: a 60 mm x 30 mm
                 pageSize = new PageSize(170, 85);
             }
 
@@ -180,51 +179,6 @@ public class HandlersEtiquetasApple
             document.Add(p9);
             document.Add(imgQR);
 
-            /*Table tbHeader = new Table(UnitValue.CreatePercentArray(new float[] { 60, 20 }));
-
-            tbHeader.AddCell(new Cell().Add(new Paragraph(String.Empty).SetFontSize(11).SetTextAlignment(TextAlignment.CENTER).SetBold().SetMinHeight(15).SetMaxHeight(15).SetMaxWidth(160))
-                                              .Add(new Paragraph("Rutas").SetMultipliedLeading(1).SetFontSize(10).SetTextAlignment(TextAlignment.LEFT).SetMinHeight(12).SetMaxHeight(12).SetMaxWidth(160)).SetBorder(Border.NO_BORDER))
-                            .SetMinHeight(35).SetMaxHeight(35).SetBorder(Border.NO_BORDER);
-
-
-            tbHeader.AddCell(new Cell().Add(new iText.Layout.Element.Image(logo).SetAutoScale(true).SetPadding(0).SetMargins(2, 5, 0, 0).SetHorizontalAlignment(HorizontalAlignment.RIGHT)).SetBorder(Border.NO_BORDER))
-                .SetMinHeight(75).SetMaxHeight(75);
-            tbHeader.SetWidth(UnitValue.CreatePercentValue(100));
-            tbHeader.SetHeight(UnitValue.CreatePercentValue(40));
-            tbHeader.SetMargins(6, 2, 0, 2);
-
-            document.Add(tbHeader);*/
-
-
-            /*Table tbBody = new Table(UnitValue.CreatePercentArray(new float[] { 50, 50 }));
-            tbBody.AddCell(new Cell().Add(new Paragraph("Numero de Viaje:").SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-            tbBody.AddCell(new Cell().Add(new Paragraph(etiqueta.Nro_Viaje).SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-
-            tbBody.AddCell(new Cell().Add(new Paragraph("# Bultos:").SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-            tbBody.AddCell(new Cell().Add(new Paragraph(etiqueta.Cantidad_Bultos).SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-
-            tbBody.AddCell(new Cell().Add(new Paragraph("# Ordenes:").SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-            tbBody.AddCell(new Cell().Add(new Paragraph(etiqueta.Cantidad_Ordenes).SetFontSize(13).SetTextAlignment(TextAlignment.CENTER).SetBold()));
-
-            tbBody.SetWidth(UnitValue.CreatePercentValue(100));
-            tbBody.SetMargins(0, 2, 0, 2);
-
-            document.Add(tbBody);*/
-
-
-
-            /*Cell celdaCodigoBarras = (new Cell().Add(codigoQR.SetAutoScale(true).SetMargins(4, 0, 0, 0).SetHorizontalAlignment(HorizontalAlignment.CENTER)));
-            Table tbFooter = new Table(UnitValue.CreatePercentArray(new float[] { 50, 50 }));
-            tbFooter.AddCell(new Cell()
-                .Add(new Paragraph("Fecha ruteo: " + "01-01-2024").SetFontSize(11).SetTextAlignment(TextAlignment.LEFT).SetBold().SetMargins(15, 0, 0, 2))
-                .Add(new Paragraph("Fecha estimada salida: " + "01-01-2024").SetFontSize(11).SetTextAlignment(TextAlignment.LEFT).SetBold().SetMargins(15, 0, 0, 2))
-                );
-            tbFooter.AddCell(celdaCodigoBarras.SetBorder(Border.NO_BORDER));
-            tbFooter.SetWidth(UnitValue.CreatePercentValue(100));
-            tbFooter.SetMargins(4, 2, 0, 2);
-
-            document.Add(tbFooter);*/
-
             pdf.Close();
 
             string fileName = System.IO.Path.GetFileName(ruta);
@@ -289,51 +243,6 @@ public class HandlersEtiquetasApple
                 document.Add(p5);
                 document.Add(p6);
                 document.Add(p7);
-
-                /*Table tbHeader = new Table(UnitValue.CreatePercentArray(new float[] { 60, 40 }));
-
-                tbHeader.AddCell(new Cell().Add(new Paragraph("Viaje:").SetFontSize(13).SetTextAlignment(TextAlignment.LEFT).SetBold().SetMinHeight(15).SetMaxHeight(40).SetMaxWidth(400))
-                                                    .SetBorder(Border.NO_BORDER))
-                                .SetMinHeight(35).SetMaxHeight(35);
-
-                tbHeader.AddCell(new Cell().Add(new Paragraph("Parada:").SetFontSize(13).SetTextAlignment(TextAlignment.LEFT).SetBold().SetMinHeight(15).SetMaxHeight(40).SetMaxWidth(400).SetMarginLeft(-25))
-
-                                         .SetBorder(Border.NO_BORDER))
-                     .SetMinHeight(35).SetMaxHeight(35);
-
-                tbHeader.SetWidth(UnitValue.CreatePercentValue(100));
-                tbHeader.SetHeight(UnitValue.CreatePercentValue(100));
-                tbHeader.SetMargins(6, 1, 0, 6);
-
-                document.Add(tbHeader);*/
-
-                /*int fontSizeParadas = 50;
-                var vAlignBultos = VerticalAlignment.TOP;
-
-                Table tbBody = new Table(UnitValue.CreatePercentArray(new float[] { 50, 50 }));
-                tbBody.AddCell(new Cell().Add(new Paragraph(etiqueta.Nro_Viaje.PadLeft(6, '0')).SetVerticalAlignment(vAlignBultos).SetFontSize(30).SetTextAlignment(TextAlignment.LEFT).SetBold()).SetMinHeight(100).SetMaxHeight(100).SetMaxWidth(50).SetBorder(Border.NO_BORDER));
-                tbBody.AddCell(new Cell());
-                tbBody.AddCell(new Cell().Add(new Paragraph(String.Format("{0}/{1}", etiqueta.Parada.PadLeft(2, '0'), etiqueta.CantParadasTotales.PadLeft(2, '0'))).SetVerticalAlignment(vAlignBultos).SetFontSize(fontSizeParadas).SetTextAlignment(TextAlignment.LEFT).SetBold()).SetMinHeight(100).SetMaxWidth(50).SetMaxHeight(100).SetBorder(Border.NO_BORDER));
-                tbBody.SetWidth(UnitValue.CreatePercentValue(100));
-                tbBody.SetMargins(6, 1, 0, 6);
-
-                document.Add(tbBody);*/
-
-                /*int fontSizeFooter = 14;
-
-                Table tbFooter = new Table(UnitValue.CreatePercentArray(new float[] { 300, 60 }));
-
-                tbFooter.AddCell(new Cell().Add(new Paragraph(String.Format("Intento: {0}", etiqueta.NroReintento)).SetFontSize(fontSizeFooter).SetTextAlignment(TextAlignment.LEFT).SetMinHeight(15).SetMaxHeight(500).SetMaxWidth(800))
-                                                    .Add(new Paragraph(String.Format("Parcel ID: {0}", etiqueta.ParcelId.PadLeft(10, '0'))).SetMultipliedLeading(1).SetFontSize(fontSizeFooter).SetTextAlignment(TextAlignment.LEFT).SetBold().SetMinHeight(12).SetMaxHeight(500).SetMaxWidth(800))
-                                                    .Add(new Paragraph(String.Format("Fecha Viaje: {0}", etiqueta.FechaViaje)).SetMultipliedLeading(1).SetFontSize(fontSizeFooter).SetTextAlignment(TextAlignment.LEFT).SetMinHeight(12).SetMaxHeight(500).SetMaxWidth(3000))
-                                                    .SetBorder(Border.NO_BORDER))
-                                .SetMinHeight(35).SetMaxHeight(60);
-
-                tbFooter.SetWidth(UnitValue.CreatePercentValue(100));
-                tbFooter.SetHeight(UnitValue.CreatePercentValue(100));
-                tbFooter.SetMargins(6, 1, 0, 6);
-
-                document.Add(tbFooter);*/
                 addNewPage = true;
             }
 
